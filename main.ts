3aad93871dda4f8d03739747a8bd686b08ea7759
camilovillotacfk <118937623+camilovillotacfk@users.noreply.github.com>
2022-11-25 16:43:31 -0500

    Versión 1.0.0

--- main.ts
+++ main.ts
@@ -3,6 +3,11 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(niños)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    niñas = 0
+    niños = 0
+    total = 0
+})
 input.onButtonPressed(Button.AB, function () {
     total = niñas + niños
     basic.showNumber(total)
@@ -13,12 +18,7 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(niñas)
     basic.clearScreen()
 })
-input.onGesture(Gesture.Shake, function () {
-    niñas = 0
-    niños = 0
-    total = 0
-})
-let niñas = 0
 let total = 0
+let niñas = 0
 let niños = 0
 basic.showString("Hola")
