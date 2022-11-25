753eeb2c3e11b38c7159de573ef3133b70441472
camilotorrescfk2022 <118938509+camilotorrescfk2022@users.noreply.github.com>
2022-11-25 16:35:58 -0500

    Botón B incrementa la variable niñas y botón A+B muestra total

--- main.ts
+++ main.ts
@@ -3,5 +3,17 @@ input.onButtonPressed(Button.A, function () {
     basic.showNumber(niños)
     basic.clearScreen()
 })
+input.onButtonPressed(Button.AB, function () {
+    total = niñas + niños
+    basic.showNumber(total)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    niñas += 1
+    basic.showNumber(niñas)
+    basic.clearScreen()
+})
+let niñas = 0
+let total = 0
 let niños = 0
 basic.showString("Hello!")
